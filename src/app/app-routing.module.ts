59ce6224936756cbb84e6273bf7b2486dc792347
Sandeep Singh <sunny9969888992@gmail.com>
2019-09-06 13:37:07 -0500

    Changes Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { FileManagerComponent } from './file-manager/file-manager.component';
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { MasterOneComponent } from './master-one/master-one.component';
 
 
 
 const routes: Routes = [//{path:'',redirectTo:'/app',pathMatch:'full'},
-                        //{path: 'app',component: AppComponent},
+                        {path: 'Master1',component: MasterOneComponent},
                         {path: 'fileManager',component: FileManagerComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
 export const routingComponent=[FileManagerComponent,AppComponent]
